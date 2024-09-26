b017db368731c047c8d8a14d5d8bf83065755bba
WarinCode <warinsaipanya471@outlook.co.th>
2024-09-26 18:38:06 +0700

    Add logical code

--- src/main/java/com/mycompany/java/project/JavaProject.java
+++ src/main/java/com/mycompany/java/project/JavaProject.java
@@ -4,9 +4,14 @@ import com.mycompany.java.project.classes.User;
 import com.mycompany.java.project.db.Authorization;
 import com.mycompany.java.project.db.Database;
 import com.mycompany.java.project.classes.customs.exceptions.JBookException;
+import com.mycompany.java.project.pages.Login;
 
 public class JavaProject {
     public static void main(String[] args) {
+        if(!Authorization.isLoggedIn){
+            Login login = new Login();
+        } else {
 
+        }
     }
 }
--- src/main/java/com/mycompany/java/project/classes/utils/Helper.java
+++ src/main/java/com/mycompany/java/project/classes/utils/Helper.java
@@ -5,7 +5,16 @@ public final class Helper {
     public static String getSingleQuotes(String value){
         return "'" + value + "'";
     }
+
     public static String getDoubleQuotes(String value){
         return "\"" + value + "\"";
+    }
+
+    public static String getString(char []characters){
+        String temp = "";
+        for(char character : characters){
+            temp += character;
+        }
+        return temp;
     }
 }
